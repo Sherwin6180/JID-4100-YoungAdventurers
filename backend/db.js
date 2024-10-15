96b823488594659b7b8c9a66cbe66afc0b5aa991
Charlieee0510 <zhouyuanhong0510@gmail.com>
2024-10-15 15:58:20 -0400

    Yuanhong test the local database

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
   port     : '3306',
   user     : 'root',
   // password : '12345678',
-  password: '863902025',
+  password: '863902025', // Yuanhong local password
   database: 'evaluation'
 });
 
