ee8d6321cc552bc38d0502411db4cd8abe5f5016
Sherwin6180 <sherwin6180@gmail.com>
2024-10-17 00:22:19 -0400

    added free response question type; developed apis for creating questions and fetching questions

--- backend/routes/assignment.js
+++ backend/routes/assignment.js
@@ -5,5 +5,7 @@ const assignmentController = require('../controllers/assignmentController');
 router.post('/createAssignment', assignmentController.createAssignment);
 router.get('/fetchAssignments/:courseID/:semester/:sectionID', assignmentController.fetchAssignments);
 router.delete('/removeAssignment', assignmentController.removeAssignment);
+router.post('/addQuestion', assignmentController.addQuestion);
+router.get('/fetchQuestions/:assignmentID', assignmentController.fetchQuestions);
 
 module.exports = router;
